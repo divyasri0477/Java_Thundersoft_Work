922e75029e8ce9866150a9eaccec58bb3994918c
divyasri0477 <divyasrilara@gmail.com>
2025-02-21 15:07:48 +0530

    ...

---
+++ Triangles.java
@@ -0,0 +1,24 @@
+public import java.io.*;
+public class Triangles{
+    public static void StartRightTriangle(int n)
+    {
+        int a,b;
+        for(a=0;a<n;a++){
+            for(b=0;b<=a;b++){
+                System.out.print("*");ṇ
+            }
+                System.out.println();
+            }
+        }
+        
+        public static void main(String args[])
+          {
+        
+            int k = 5;
+            StarRightTriangle(k);
+        }
+    }
+
+ {
+    
+}
